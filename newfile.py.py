1dfc70154b48257a75c60abf93adf1baffe5cf9b
Sobia Ghulam Rasool <moeedk377@gmail.com>
2026-01-16 04:04:47 -0800

    Add files via upload

---
+++ newfile.py.py
@@ -0,0 +1,34 @@
+# Find area of square
+# formula : area =side*side
+side=int(input("side of square"))
+area=side*side
+print("Area of a square",area)
+#output
+side of a square  6
+Area of a square  36
+
+
+# Find area of a triangle
+# formula: area=0.5*base*height
+base=int(input("base of a triangle"))
+height=float(input("height of a triangle"))
+area=0.5*base*height
+print("Area of a triangle",area)
+# output
+base of a triangle 10
+height of a triangle 9.8
+Area of a triangle  49.0
+
+
+# Find area of a circle
+# formula: π*r*r
+# π=3.14
+# r= radius of a circle
+r=float(input("radius of a circle"))
+area=3.14*r*r
+print("Area of a circle",area)
+# output
+radius of a circle 3.8
+Area of a circle  45.3416
+
+
